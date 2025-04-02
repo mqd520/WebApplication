34f54509e2eff6db8d5b417de4dc0b412b6150fb
Mqd <1506816370@qq.com>
2025-04-03 02:22:31 +0800

    11

--- WebApplication1/Controllers/LoggerController.cs
+++ WebApplication1/Controllers/LoggerController.cs
@@ -21,5 +21,18 @@ namespace WebApplication1.Controllers
             _logger.LogInformation("This is a [Info] log message");
             return Content("Info");
         }
+
+        public IActionResult Error()
+        {
+            try
+            {
+                int n = Convert.ToInt32("nnn");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "This is a Error log message");
+            }
+            return Content("Error");
+        }
     }
 }
